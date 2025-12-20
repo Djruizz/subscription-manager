a4e211cb66651454cbf1be365e1ed578c1b40c92
djruizz <dario.jruiz31@gmail.com>
2025-12-19 22:18:00 -0600

    Responsive sidebar

--- app/components/layout/SideBarNav.vue
+++ app/components/layout/SideBarNav.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import { type NavigationMenuItem } from "@nuxt/ui";
 import { type DropdownMenuItem } from "@nuxt/ui";
-
+const props = defineProps<{
+  collapsed: boolean;
+}>();
 const { signOut } = useAuth();
 const user = useSupabaseUser();
 
@@ -13,6 +15,7 @@ const navItems = ref<NavigationMenuItem[]>([
   { label: "Calendar", to: "/", icon: "i-lucide-calendar" },
   { label: "Analytics", to: "/", icon: "i-lucide-chart-line" },
 ]);
+
 const userItems = ref<DropdownMenuItem[]>([
   [{ label: userName, icon: "i-lucide-user", type: "label" }],
   [
@@ -36,61 +39,57 @@ const handleDropdownClick = async (value: string) => {
 };
 </script>
 <template>
-  <div>
-    <!-- Logo -->
-    <NuxtLink to="/dashboard">
-      <div class="py-6 bg-slate-900 rounded-lg">
-        <img
-          src="../../assets/img/LogoOutlay.png"
-          alt="Logo de la app"
-          class="h-20 mx-auto"
-        />
-      </div>
-    </NuxtLink>
-
-    <!-- Navigation -->
-
+  <div class="flex flex-col justify-between h-full p-3">
     <UNavigationMenu
       :items="navItems"
       orientation="vertical"
-      class="py-6"
+      class=""
       variant="pill"
       :ui="{
-        link: 'text-md',
+        link: collapsed ? 'justify-center' : 'text-md',
+        linkLabel: collapsed ? 'hidden' : '',
       }"
     />
-  </div>
 
-  <!-- Bottom actions -->
-  <div class="space-y-2">
-    <UButton
-      label="Settings"
-      class="w-full"
-      icon="i-lucide-settings"
-      size="lg"
-      color="neutral"
-      variant="outline"
-    />
-    <UDropdownMenu
-      class="w-full justify-between cursor-pointer"
-      :items="userItems"
-      :content="{ align: 'center', side: 'top' }"
-      @select="handleDropdownClick"
+    <!-- Bottom actions -->
+    <div
+      class="space-y-2 text-center flex flex-col justify-center items-center"
     >
       <UButton
-        variant="soft"
+        label="Settings"
+        class=""
+        :class="collapsed ? 'justify-center' : 'text-md w-full'"
+        icon="i-lucide-settings"
+        size="md"
         color="neutral"
-        trailing-icon="i-lucide-chevron-up"
+        variant="outline"
+        :ui="{
+          label: collapsed ? 'hidden' : '',
+        }"
+      />
+      <UDropdownMenu
+        class="justify-between cursor-pointer"
+        :items="userItems"
+        :content="{ align: 'center', side: 'right' }"
+        @select="handleDropdownClick"
       >
-        <UUser
-          name="Dario"
-          :avatar="{
-            src: '',
-            icon: 'i-lucide-user',
-            class: 'border border-emerald-500',
-          }"
-        />
-      </UButton>
-    </UDropdownMenu>
+        <UButton
+          variant="soft"
+          color="neutral"
+          class="w-full"
+          :trailing-icon="collapsed ? '' : 'i-lucide-chevron-right'"
+        >
+          <UUser
+            :name="collapsed ? '' : 'Dario'"
+            :avatar="{
+              src: '',
+              icon: 'i-lucide-user',
+              class: 'border border-emerald-500',
+            }"
+            :class="collapsed ? 'gap-0' : 'gap-2'"
+          />
+        </UButton>
+      </UDropdownMenu>
+    </div>
   </div>
 </template>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,34 +1,99 @@
 <script setup lang="ts">
-
-const { signOut } = useAuth();
+const isCollapsed = ref(false);
+const isMobileOpen = ref(false);
 </script>
 
 <template>
   <UPage
-    class="h-full bg-linear-to-b from-slate-900 via-slate-900 to-slate-950"
+    class="bg-linear-to-b from-slate-950 via-slate-900 to-slate-950 min-h-screen"
   >
-    <div class="grid grid-cols-12 gap-6 p-6 min-h-screen">
-      <aside
+    <div class="flex min-h-screen overflow-hidden lg:overflow-visible">
+      <!-- <aside
         class="col-span-12 lg:col-span-2 bg-slate-800 rounded-xl p-4 flex flex-col justify-between"
       >
-        <LayoutSideBarNav />
+        
+      </aside> -->
+      <aside
+        class="hidden lg:flex flex-col border-r border-slate-800 bg-slate-900 sticky h-[calc(100vh-2.5rem)] top-5 m-5 transition-translate duration-300 ease-in-out rounded-xl"
+        :class="[isCollapsed ? 'w-20 ' : 'w-64']"
+      >
+        <div
+          class="h-20 flex items-center justify-center border-b border-slate-800"
+        >
+          <NuxtLink to="/dashboard" v-if="isCollapsed">
+            <img
+              src="../assets/img/IconoLogoOutlay.png"
+              alt="Logo de la app"
+              class="h-10 mx-auto"
+            />
+          </NuxtLink>
+          <NuxtLink to="/dashboard" v-else>
+            <img
+              src="../assets/img/LogoOutlay.png"
+              alt="Logo de la app"
+              class="h-15 mx-auto"
+            />
+          </NuxtLink>
+        </div>
+
+        <div class="flex-1 py-4 overflow-y-auto overflow-x-hidden">
+          <LayoutSideBarNav :collapsed="isCollapsed" />
+        </div>
+
+        <div class="p-4 border-t border-slate-800 flex justify-center">
+          <UButton
+            color="neutral"
+            variant="ghost"
+            :icon="
+              isCollapsed
+                ? 'i-heroicons-chevron-double-right-20-solid'
+                : 'i-heroicons-chevron-double-left-20-solid'
+            "
+            @click="isCollapsed = !isCollapsed"
+          />
+        </div>
       </aside>
 
+      <USlideover
+        v-model:open="isMobileOpen"
+        side="left"
+        class="w-64 bg-slate-900 max-h-screen"
+        :overlay="false"
+      >
+        <template #title>
+          <NuxtLink to="/dashboard">
+            <img
+              src="../assets/img/LogoOutlay.png"
+              alt="Logo de la app"
+              class="h-15 mx-auto"
+            />
+          </NuxtLink>
+        </template>
+        <template #body>
+          <LayoutSideBarNav :collapsed="false" />
+        </template>
+      </USlideover>
       <!-- MAIN CONTENT -->
-      <UMain class="col-span-12 lg:col-span-10 space-y-6">
-        <!-- HEADER -->
-        <UHeader>
-          <template #title>
+      <UMain class="flex-1 min-w-0">
+        <UHeader class="m-5 rounded-xl top-5">
+          <template #left>
+            <UButton
+              icon="i-heroicons-bars-3"
+              color="neutral"
+              variant="ghost"
+              class="lg:hidden mr-4"
+              @click="isMobileOpen = true"
+            />
             <h1 class="text-2xl font-semibold">Panel General</h1>
           </template>
-
           <template #right>
-            <div class="flex items-center gap-3">
-              <LayoutHeaderRightButtons/>
-            </div>
+            <LayoutHeaderRightButtons />
           </template>
         </UHeader>
-        <slot></slot>
+
+        <div class="p-4 md:p-6">
+          <slot></slot>
+        </div>
       </UMain>
     </div>
   </UPage>
